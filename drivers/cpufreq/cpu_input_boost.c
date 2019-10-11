e43f3d4505401b830523f158e2bc5bbd7eac7064
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-10-11 19:56:25 +0530

    cpufreq: do not bias/boost cpu/tasks when screen is off
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -11,6 +11,7 @@
 #include <linux/kthread.h>
 #include <linux/moduleparam.h>
 #include <linux/msm_drm_notify.h>
+#include <linux/sched.h>
 #include <linux/power_hal.h>
 #include <linux/slab.h>
 #include <linux/version.h>
@@ -460,7 +461,9 @@ static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
 	if (*blank == MSM_DRM_BLANK_UNBLANK_CUST) {
 		set_bit(SCREEN_ON, &b->state);
 		__cpu_input_boost_kick_max(b, wake_boost_duration);
+		disable_schedtune_boost("top-app", false);
 	} else if (*blank == MSM_DRM_BLANK_POWERDOWN_CUST) {
+		disable_schedtune_boost("top-app", true);
 		clear_bit(SCREEN_ON, &b->state);
 		wake_up(&b->boost_waitq);
 	}
