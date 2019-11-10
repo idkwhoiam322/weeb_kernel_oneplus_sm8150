3d6d505c1c5109670de64b1710706a5ad6fe9277
Park Ju Hyung <qkrwngud825@gmail.com>
2019-11-10 22:00:43 +0900

    op_touchscreen: avoid unnecessary heap allocations
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/input/oneplus_touchscreen/touchpanel_common_driver.c
+++ drivers/input/oneplus_touchscreen/touchpanel_common_driver.c
@@ -649,7 +649,7 @@ static void tp_touch_handle(struct touchpanel_data *ts)
 	int i = 0;
 	uint8_t finger_num = 0, touch_near_edge = 0;
 	int obj_attention = 0;
-	struct point_info *points;
+	struct point_info points[10];
 	struct corner_info corner[4];
 	static struct point_info last_point = {.x = 0,.y = 0 };
 	static int touch_report_num = 0;
@@ -664,11 +664,7 @@ static void tp_touch_handle(struct touchpanel_data *ts)
 		return;
 	}
 
-	points = kzalloc(sizeof(struct point_info) * ts->max_num, GFP_KERNEL);
-	if (!points) {
-		TPD_INFO("points kzalloc failed\n");
-		return;
-	}
+	memset(points, 0, sizeof(points));
 	memset(corner, 0, sizeof(corner));
 	if (ts->reject_point) {	//sensor will reject point when call mode.
 		if (ts->touch_count) {
@@ -686,7 +682,6 @@ static void tp_touch_handle(struct touchpanel_data *ts)
 #endif
 			input_sync(ts->input_dev);
 		}
-		kfree(points);
 		return;
 	}
 	obj_attention =
@@ -797,7 +792,6 @@ static void tp_touch_handle(struct touchpanel_data *ts)
 	}
 	input_sync(ts->input_dev);
 	ts->touch_count = finger_num;
-	kfree(points);
 }
 
 static void tp_btnkey_release(struct touchpanel_data *ts)
