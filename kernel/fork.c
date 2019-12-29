8418c06e2c0eccaae017b92e6163f1305093c462
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-12-29 17:57:12 +0300

    nuke is_zygote_pid

--- kernel/fork.c
+++ kernel/fork.c
@@ -2111,12 +2111,6 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 1000 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid)) {
-		cpu_input_boost_kick_max(1000);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
-	}
-
 	/*
 	 * Determine whether and which event to report to ptracer.  When
 	 * called from kernel_thread or CLONE_UNTRACED is explicitly
