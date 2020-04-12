e587d9bb209244d763a9ffaa13d2df110dec51da
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-04-12 15:16:52 +0530

    Partially revert "drm: msm: Add separate sysfs node for FOD dimming alpha"
    When the aforementioned commit was introduced, ROMs that did not use it had
    slower FP as usual, sometimes as slow as 2-3x. However, this commit is needed
    to ensure compatibility of FOD support on maximum ROMs. Partially revert it and
    add back the condition for 'op_dimlayer_bl_enable' to ensure that normal speeds
    are retained.
    
    This will likely break DC dimming on custom ROMs but that's a sacrifice
    I'm willing to make as having working FOD is a higher priority until all ROMs
    unify their implementations.
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -3433,7 +3433,7 @@ int oneplus_get_panel_brightness_to_alpha(void)
 		return 0;
 	if (oneplus_panel_alpha)
 		return oneplus_panel_alpha;
-	if (display->panel->dim_status)
+	if (!op_dimlayer_bl_enable || display->panel->dim_status)
 		return brightness_to_alpha(display->panel->hbm_backlight);
 	else
 	return bl_to_alpha_dc(display->panel->hbm_backlight);
