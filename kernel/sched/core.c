95cdd05ac7fb4e10876c7904c229c56f2a15be3d
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-06-21 13:30:39 +0530

    Revert "sched: Improve the scheduler"
    This reverts commit d3f0fbc2ee42c5f416cb410341c00693c17cb425.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4943,7 +4943,6 @@ unsigned int sched_lib_mask_force;
 bool is_sched_lib_based_app(pid_t pid)
 {
 	const char *name = NULL;
-	char *lib_list, *libname;
 	struct vm_area_struct *vma;
 	char path_buf[LIB_PATH_LENGTH];
 	bool found = false;
@@ -4953,14 +4952,11 @@ bool is_sched_lib_based_app(pid_t pid)
 	if (strnlen(sched_lib_name, LIB_PATH_LENGTH) == 0)
 		return false;
 
-	lib_list = kstrdup(sched_lib_name, GFP_KERNEL);
-
 	rcu_read_lock();
 
 	p = find_process_by_pid(pid);
 	if (!p) {
 		rcu_read_unlock();
-		kfree(lib_list);
 		return false;
 	}
 
@@ -4980,12 +4976,10 @@ bool is_sched_lib_based_app(pid_t pid)
 			if (IS_ERR(name))
 				goto release_sem;
 
-			while ((libname = strsep(&lib_list, ","))) {
-				if (strnstr(name, libname,
+			if (strnstr(name, sched_lib_name,
 					strnlen(name, LIB_PATH_LENGTH))) {
-					found = true;
-					break;
-				}
+				found = true;
+				break;
 			}
 		}
 	}
@@ -4995,7 +4989,6 @@ release_sem:
 	mmput(mm);
 put_task_struct:
 	put_task_struct(p);
-	kfree(lib_list);
 	return found;
 }
 
