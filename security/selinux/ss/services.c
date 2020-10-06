2cd3639e506e78aa8f5cdbe7ac5c8f2e6d233759
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-10-06 23:24:51 +0530

    Revert "selinux: don't require auditing"
    This reverts commit 0b6975b56b74a2f4cb6fc1c4b15246095cdec016.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3554,7 +3554,6 @@ out:
 	return match;
 }
 
-#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3577,7 +3576,6 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
-#endif
 
 #ifdef CONFIG_NETLABEL
 /**
